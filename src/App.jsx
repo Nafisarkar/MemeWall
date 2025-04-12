ff60da6b1cbfa04edb2e416a3672c8c939ae3ea5
Shaon An Nafi <sarkarnafe@gmail.com>
2025-04-12 17:15:18 +0600

    feat: implement smooth scrolling with Lenis and add scrollToSection functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import Navbar from "./components/ui/navbar";
+import { useEffect, useRef } from "react";
 import Homepage from "./pages/homepage";
 import { Toaster } from "@/components/ui/toaster";
+import Lenis from "lenis";
 function App() {
+  const lenis = useRef(null);
+
+  useEffect(() => {
+    // Initialize Lenis
+    lenis.current = new Lenis({
+      duration: 0.6, // Control the duration of the scroll
+      easing: (t) => 1 - Math.pow(1 - t, 3), // Cubic easing for smooth stop
+      smooth: true,
+      smoothTouch: true, // Enable smooth scrolling on touch devices
+    });
+
+    const animate = (time) => {
+      lenis.current.raf(time);
+      requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate);
+
+    // Cleanup on unmount
+    return () => {
+      lenis.current.destroy();
+    };
+  }, []);
+
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    lenis.current.scrollTo(element);
+  };
   return (
     <>
       <Navbar />
